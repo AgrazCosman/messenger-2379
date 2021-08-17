9c2b54fd8276c6c9f7667354fe2bd0824d028249
S. Cosman Agraz <88801117+AgrazCosman@users.noreply.github.com>
2021-08-17 14:47:36 -0400

    Displays messages in reverse order (newest at the bottom)

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -6,17 +6,21 @@ import moment from "moment";
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
 
+  const messageArray = messages.map((message) => {
+    const time = moment(message.createdAt).format("h:mm");
+
+    return message.senderId === userId ? (
+      <SenderBubble key={message.id} text={message.text} time={time} />
+    ) : (
+      <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
+    );
+  });
+
+  const messageReverseOrder = [...messageArray].reverse();
+
   return (
     <Box>
-      {messages.map((message) => {
-        const time = moment(message.createdAt).format("h:mm");
-
-        return message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} />
-        ) : (
-          <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
-        );
-      })}
+      {messageReverseOrder}
     </Box>
   );
 };
