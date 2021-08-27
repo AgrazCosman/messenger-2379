d2771d4412adc964596eb05990ffcdbb7931a113
S. Cosman Agraz <88801117+AgrazCosman@users.noreply.github.com>
2021-08-27 14:34:26 -0400

    send patch request when selecting active chat

--- client/src/components/Sidebar/Chat.js
+++ client/src/components/Sidebar/Chat.js
@@ -3,6 +3,7 @@ import { Box } from "@material-ui/core";
 import { BadgeAvatar, ChatContent } from "../Sidebar";
 import { makeStyles } from "@material-ui/core/styles";
 import { setActiveChat } from "../../store/activeConversation";
+import { patchConversation } from "../../store/utils/thunkCreators";
 import { connect } from "react-redux";
 
 const useStyles = makeStyles((theme) => ({
@@ -26,6 +27,7 @@ const Chat = (props) => {
 
   const handleClick = async (conversation) => {
     await props.setActiveChat(conversation.otherUser.username);
+    await props.patchConversation(conversation);
   };
 
   return (
@@ -45,7 +47,11 @@ const mapDispatchToProps = (dispatch) => {
   return {
     setActiveChat: (id) => {
       dispatch(setActiveChat(id));
+    },
+    patchConversation: (convo) => {
+        dispatch(patchConversation(convo));
     }
+
   };
 };
 
